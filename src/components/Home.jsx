5d47f8a309685a1cc731e6e6e534fe6552c51fc5
Paresh Madav <madavparesh08@gmail.com>
2025-09-19 20:19:55 +0530

    Aise hi timepass kar raha hu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-
-export default function Home() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-5xl font-bold">Employee Dashboard 🚀</h1>
+import React from 'react';
+import { DatePicker } from 'antd';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+dayjs.extend(customParseFormat);
+const dateFormat = 'YYYY-MM-DD';
+const App = () => (
+  <div className='flex items-center justify-center min-h-screen'>
+  <DatePicker className='flex justify'
+    defaultValue={dayjs('2019-09-03', dateFormat)}
+    minDate={dayjs('2019-08-01', dateFormat)}
+    maxDate={dayjs('2020-10-31', dateFormat)}
+  />
+  <div className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
     </div>
-      
-    
-  )
-}
+  </div>
+</div>
+  </div>
+);
+export default App;
