c03472d8dcdadf0ffc4533bc292679b5a509d56a
Paresh Madav <madavparesh08@gmail.com>
2025-09-19 18:14:56 +0530

    Initial commit - Vite + Tailwind setup

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-5xl font-bold">Employee Dashboard 🚀</h1>
+    </div>
+      
+    
+  )
+}
